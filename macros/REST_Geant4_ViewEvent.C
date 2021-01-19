8e962d9652ef80a2b236dc2bbbea3307a04155a2
j.a.galan.81@gmail.com <j.a.galan.81@gmail.com>
2021-01-19 11:52:48 +0100

    REST_Geant4_ViewEvent. Fixed the name of class TRestGeant4EventViewer

--- macros/REST_Geant4_ViewEvent.C
+++ macros/REST_Geant4_ViewEvent.C
@@ -11,7 +11,7 @@
 //***
 //*******************************************************************************************************
 Int_t REST_Geant4_ViewEvent(TString fName) {
-    TRestBrowser* browser = new TRestBrowser("TRestG4EventViewer");
+    TRestBrowser* browser = new TRestBrowser("TRestGeant4EventViewer");
 
     TRestEvent* eve = new TRestGeant4Event();
     browser->SetInputEvent(eve);
