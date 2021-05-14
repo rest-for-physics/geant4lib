e6922b2a3ca2650ac07ead163ef7e86445e131c2
Luis Obis <luis.antonio.obis@gmail.com>
2021-05-14 11:08:51 +0200

    TRestGeant4Track - Refactoring, removed GetProcessID, GetProcessName, isXXX methods

--- inc/TRestGeant4Track.h
+++ inc/TRestGeant4Track.h
@@ -18,14 +18,12 @@
 
 #include <TArrayI.h>
 #include <TColor.h>
+#include <TObject.h>
 #include <TRestGeant4Hits.h>
 #include <TString.h>
 #include <TVector3.h>
 
 #include <iostream>
-#include <vector>
-
-#include "TObject.h"
 
 // Perhaps there might be need for a mother class TRestTrack (if there is future
 // need)
@@ -102,141 +100,6 @@ class TRestGeant4Track : public TObject {
     }
 
     void RemoveHits() { fHits.RemoveHits(); }
-
-    // TODO move this to a namespace header??
-    Int_t GetProcessID(TString pcsName);
-    TString GetProcessName(Int_t id);
-
-    Bool_t isRadiactiveDecay() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 11) return true;
-        return false;
-    }
-    Bool_t isPhotoElectric() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 3) return true;
-        return false;
-    }
-    Bool_t isCompton() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 7) return true;
-        return false;
-    }
-    Bool_t isBremstralung() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 5) return true;
-        return false;
-    }
-
-    Bool_t ishadElastic() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 36) return true;
-        return false;
-    }
-    Bool_t isneutronInelastic() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 37) return true;
-        return false;
-    }
-    Bool_t isnCapture() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 38) return true;
-        return false;
-    }
-
-    Bool_t ishIoni() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 33) return true;
-        return false;
-    }
-    Bool_t isphotonNuclear() {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if (GetHits()->GetHitProcess(n) == 42) return true;
-        return false;
-    }
-    // Processes in active volume
-    Bool_t isRadiactiveDecayInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 11) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-    Bool_t isPhotoElectricInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 3) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-    Bool_t isPhotonNuclearInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 42) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-
-    Bool_t isComptonInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 7) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-    Bool_t isBremstralungInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 5) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-
-    Bool_t isHadElasticInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 36) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-    Bool_t isNeutronInelasticInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 37) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-
-    Bool_t isNCaptureInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 38) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-
-    Bool_t isHIoniInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitProcess(n) == 33) && (GetHits()->GetHitVolume(n)) == volID) return true;
-        return false;
-    }
-
-    Bool_t isAlphaInVolume(Int_t volID) {
-        if (GetParticleName() == "alpha") {
-            for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-                if ((GetHits()->GetHitVolume(n)) == volID) return true;
-        }
-        return false;
-    }
-
-    Bool_t isNeutronInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitVolume(n) == volID) && (GetParticleName() == "neutron")) return true;
-        return false;
-    }
-
-    Bool_t isArgonInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitVolume(n) == volID) && (GetParticleName().Contains("Ar"))) return true;
-        return false;
-    }
-
-    Bool_t isNeonInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitVolume(n) == volID) && (GetParticleName().Contains("Ne"))) return true;
-        return false;
-    }
-
-    Bool_t isXenonInVolume(Int_t volID) {
-        for (int n = 0; n < GetHits()->GetNumberOfHits(); n++)
-            if ((GetHits()->GetHitVolume(n) == volID) && (GetParticleName().Contains("Xe"))) return true;
-        return false;
-    }
-    /////////////////////////////////
 
     /// Prints the track information. N number of hits to print, 0 = all
     void PrintTrack(int maxHits = 0);
