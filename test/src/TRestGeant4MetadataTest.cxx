85ce6cac4e7e92c6096e468aa8d44045228e1bc9
lobis <luis.antonio.obis@gmail.com>
2021-11-09 20:34:23 +0100

    Refactored tests, added template generator tests

--- test/src/TRestGeant4MetadataTest.cxx
+++ test/src/TRestGeant4MetadataTest.cxx
@@ -2,11 +2,13 @@
 
 #include "TRestGeant4Metadata.h"
 
-TEST(TRestGeant4Metadata, BasicTest) { EXPECT_EQ(2 * 2, 4); }
-
 TEST(TRestGeant4Metadata, DefaultParameters) {
     auto restGeant4Metadata = TRestGeant4Metadata();
     auto sectionName = restGeant4Metadata.GetSectionName();
 
     EXPECT_EQ(sectionName, "TRestGeant4Metadata");
+}
+
+TEST(TRestGeant4Metadata, Generators) {
+    auto restGeant4Metadata = TRestGeant4Metadata("generators/generator.rml");
 }
