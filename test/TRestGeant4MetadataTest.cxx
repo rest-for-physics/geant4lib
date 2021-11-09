58fa5077192bd20cd30fbe042d8fe321c2d2a10e
Luis Antonio Obis Aparicio <lobis@unizar.es>
2021-11-09 11:26:22 +0100

    Geant4Lib test with dependencies compilation works

--- test/TRestGeant4MetadataTest.cxx
+++ test/TRestGeant4MetadataTest.cxx
@@ -1,14 +1,12 @@
 #include <gtest/gtest.h>
 
-// #include "TRestGeant4Metadata.h"
+#include "TRestGeant4Metadata.h"
 
 TEST(TRestGeant4Metadata, BasicTest) { EXPECT_EQ(2 * 2, 4); }
 
-/*
 TEST(TRestGeant4Metadata, DefaultParameters) {
     auto restGeant4Metadata = TRestGeant4Metadata();
     auto sectionName = restGeant4Metadata.GetSectionName();
 
     EXPECT_EQ(sectionName, "TRestGeant4Metadata");
 }
- */
