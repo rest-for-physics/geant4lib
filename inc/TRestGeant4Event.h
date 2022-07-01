be07046e7d965fd893b3393a0bf93028063925f3
Luis Obis <luis.antonio.obis@gmail.com>
2022-07-01 13:15:31 +0200

    Added method to retrieve metadata more easily

--- inc/TRestGeant4Event.h
+++ inc/TRestGeant4Event.h
@@ -38,6 +38,8 @@
 #include <map>
 #include <utility>
 
+class TRestGeant4Metadata;
+
 /// An event class to store geant4 generated event information
 class TRestGeant4Event : public TRestEvent {
    private:
@@ -427,6 +429,8 @@ class TRestGeant4Event : public TRestEvent {
     void Initialize();
 
     void InitializeReferences(TRestRun* run) override;
+
+    const TRestGeant4Metadata* GetGeant4Metadata(const char* name = "TRestGeant4Metadata") const;
 
     /// maxTracks : number of tracks to print, 0 = all
     void PrintActiveVolumes() const;
