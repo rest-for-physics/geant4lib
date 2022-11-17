48cce8899fcbe9de87706acf211803ad48e470f1
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-17 10:50:56 +0100

    add world size information and helper methods

--- inc/TRestGeant4PrimaryGeneratorInfo.h
+++ inc/TRestGeant4PrimaryGeneratorInfo.h
@@ -90,13 +90,12 @@ TF2 EnergyAndAngularDistributionFormulasToRootFormula(const EnergyAndAngularDist
 class TiXmlElement;
 
 class TRestGeant4PrimaryGeneratorInfo {
-    ClassDef(TRestGeant4PrimaryGeneratorInfo, 1);
+    ClassDef(TRestGeant4PrimaryGeneratorInfo, 2);
 
    public:
     void Print() const;
 
    private:
-    /* Members related to spatial generator */
     /// Type of spatial generator (point, surface, volume, custom)
     TString fSpatialGeneratorType = "point";
 
@@ -109,9 +108,8 @@ class TRestGeant4PrimaryGeneratorInfo {
     /// The position of the generator for virtual, and point, generator types
     TVector3 fSpatialGeneratorPosition;
 
-    /// \brief A 3d-std::vector with the angles, measured in degrees, of a XYZ rotation
-    /// applied to the virtual generator. This rotation is used by virtualWall,
-    /// virtualCircleWall and virtualCylinder generators.
+    /// \brief A 3d-std::vector with the angles, measured in degrees, of a XYZ rotation applied to the virtual
+    /// generator. This rotation is used by virtualWall, virtualCircleWall and virtualCylinder generators.
     TVector3 fSpatialGeneratorRotationAxis;
 
     /// \brief degrees of rotation for generator virtual shape along the axis
@@ -128,35 +126,40 @@ class TRestGeant4PrimaryGeneratorInfo {
     /// \brief Defines density distribution of the generator shape. rho=F(x,y,z), in range 0~1
     TString fSpatialGeneratorSpatialDensityFunction;
 
+    /// \brief The size of the world volume (box half lengths for x, y and z)
+    TVector3 fSpatialGeneratorWorldSize;
+
    public:
-    /// \brief Returns a std::string specifying the generator type (volume, surface, point,
-    /// virtualWall, etc )
+    /// \brief Returns a std::string specifying the generator type (volume, surface, point, virtualWall, etc )
     inline TString GetSpatialGeneratorType() const { return fSpatialGeneratorType; }
 
     /// \brief Returns a std::string specifying the generator shape (point, wall, box, etc )
     inline TString GetSpatialGeneratorShape() const { return fSpatialGeneratorShape; }
 
-    /// \brief Returns the name of the GDML volume where primary events are
-    /// produced. This value has meaning only when using volume or surface
-    /// generator types.
+    /// \brief Returns the name of the GDML volume where primary events are produced. This value has meaning
+    /// only when using volume or surface generator types.
     inline TString GetSpatialGeneratorFrom() const { return fSpatialGeneratorFrom; }
 
-    /// \brief Returns a 3d-std::vector with the position of the primary event
-    /// generator. This value has meaning only when using point and virtual
-    /// generator types.
+    /// \brief Returns a 3d-std::vector with the position of the primary event generator. This value has
+    /// meaning only when using point and virtual generator types.
     inline TVector3 GetSpatialGeneratorPosition() const { return fSpatialGeneratorPosition; }
 
-    /// \brief Returns a 3d-std::vector, fGenRotation, with the XYZ rotation angle
-    /// values in degrees. This value is used by virtualWall, virtualCircleWall
-    /// and virtualCylinder generator types.
+    /// \brief Returns a 3d-std::vector, fGenRotation, with the XYZ rotation angle values in degrees. This
+    /// value is used by virtualWall, virtualCircleWall and virtualCylinder generator types.
     inline TVector3 GetSpatialGeneratorRotationAxis() const { return fSpatialGeneratorRotationAxis; }
 
     /// \brief Returns the degree of rotation
     inline Double_t GetSpatialGeneratorRotationValue() const { return fSpatialGeneratorRotationValue; }
 
-    /// \brief Returns the main spatial dimension of virtual generator.
-    /// It is the size of a  virtualBox.
+    /// \brief Returns the main spatial dimension of virtual generator. It is the size of a  virtualBox.
     inline TVector3 GetSpatialGeneratorSize() const { return fSpatialGeneratorSize; }
+
+    inline TVector3 GetSpatialGeneratorWorldSize() const { return fSpatialGeneratorWorldSize; }
+    inline Double_t GetSpatialGeneratorCosmicRadius() const { return fSpatialGeneratorWorldSize.Mag(); }
+    inline Double_t GetSpatialGeneratorCosmicSurfaceTerm() const {
+        const auto radius = GetSpatialGeneratorCosmicRadius();
+        return M_PI * radius * radius;
+    }
 
     /// \brief Returns the density function of the generator
     inline TString GetSpatialGeneratorSpatialDensityFunction() const {
